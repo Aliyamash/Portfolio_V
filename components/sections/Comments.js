446c802306a8142fd24bde3912fc6ae2bf9a3e6d
ᴍᴏʀɢᴀɴ <ali2763.mar@gmail.com>
2024-10-30 18:53:20 +0330

    fix comment section and create famuosPeople and ParentText

--- components/sections/Comments.js
+++ components/sections/Comments.js
@@ -7,10 +7,11 @@ import comment5 from '@/public/img/comment5.png'
 import comment6 from '@/public/img/comment6.png'
 import comment7 from '@/public/img/comment7.png'
 import comment8 from '@/public/img/comment8.png'
+
 export default function Comment() {
   return (
     <>
-      <div className="h-screen mt-96">
+      <div className=" mt-96">
         {/* title of Comments */}
         <div className="my-auto py-20 ">
           <h1
@@ -27,41 +28,50 @@ export default function Comment() {
         <div className="warrper1">
 
           <div className="items1 item1">
-            <Image  src={comment1} className="object-fit rounded-lg"/>
+            <Image  src={comment1} className="object-fit rounded-lg" alt="comments"/>
           </div>
 
           <div className="items1 item2">
-            <Image  src={comment2} className="object-fit rounded-lg"/>
+            <Image  src={comment2} className="object-fit rounded-lg" alt="comments" />
           </div>
 
           <div className="items1 item3">
-            <Image  src={comment3} className="object-fit rounded-lg"/>
+            <Image  src={comment3} className="object-fit rounded-lg" alt="comments"/>
           </div>
 
           <div className="items1 item4">
-            <Image  src={comment4} className="object-fit rounded-lg"/>
+            <Image  src={comment4} className="object-fit rounded-lg" alt="comments"/>
           </div>
         </div>
 
         <div  className="warrper2">
         <div className="items2 item5">
-            <Image  src={comment5} className="object-fit rounded-lg"/>
+            <Image  src={comment5} className="object-fit rounded-lg" alt="comments"/>
           </div>
 
           <div className="items2 item6">
-            <Image  src={comment6} className="object-fit rounded-lg"/>
+            <Image  src={comment6} className="object-fit rounded-lg" alt="comments"/>
           </div>
 
           <div className="items2 item7">
-            <Image  src={comment7} className=" object-fit rounded-lg"/>
+            <Image  src={comment7} className=" object-fit rounded-lg" alt="comments"/>
           </div>
 
           <div className="items2 item8">
-            <Image  src={comment8} className="object-fit rounded-lg"/>
+            <Image  src={comment8} className="object-fit rounded-lg" alt="comments"/>
           </div>
 
         </div>
-
+        {/* parent comments */}
+        <div className="flex gap-8 justify-center mt-24">
+          <div className="ptext capitalize w-1/3 text-center text-4xl px-24">
+            <p>The programmer organizes the world and God loves order</p>
+          </div>
+          <div className="ptext capitalize w-1/3 text-center text-4xl p-8">
+            <p>Enjoying seeing your art shows that you are awesome.</p>
+          </div>
+          
+        </div>
       </div>
     </>
   );
---
+++ components/sections/FamousPeople.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import BrendanEich from "@/public/img/BrendanEich.png";
+import jadi from "@/public/img/jadi.png";
+export default function FamousPeople() {
+  return (
+    <>
+      {/* Comments2 */}
+      <div className="mb-24 mt-36 text-center text-2xl capitalize">
+        <h2>And I am eager to hear the opinion of these loved ones</h2>
+      </div>
+      <div className="flex gap-8 my-24 justify-center">
+        <div>
+          <Image
+            src={BrendanEich}
+            className="famousPic1 rounded-2xl shadow-2xl shadow-gray-700"
+            height={120}
+            alt="berdan"
+          />
+        </div>
+        <div >
+          <Image
+            src={jadi}
+            className="famousPic2 rounded-2xl shadow-2xl shadow-gray-700"
+            height={120}
+            alt="jadi"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
