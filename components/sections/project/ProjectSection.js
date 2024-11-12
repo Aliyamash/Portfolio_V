3ea4f557b576d3c55bd98f0af8c5176188ed8355
ᴍᴏʀɢᴀɴ <ali2763.mar@gmail.com>
2024-11-12 19:52:12 +0330

    work on card section and creat a clear idea

--- components/sections/project/ProjectSection.js
+++ components/sections/project/ProjectSection.js
@@ -2,7 +2,7 @@ export default function ProjectSection(){
 
     return(
         <>
-        <div className="bg-project h-screen py-44">
+        <div className="bg-project h-screen pt-44 pb-60">
           <div className="mt-60">
           <p className="text-project text-center text-white text-6xl pt-24">Here are a few things I've done recently</p>
           </div>
--- components/sections/project/SkyNight.js
+++ components/sections/project/SkyNight.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Sky from "@/public/img/khalifetower.jpg";
+import rightIcon from "@/public/img/right.png";
 
 import { useRef } from "react";
 
@@ -97,6 +97,7 @@ export default function SkyNight() {
       clip7.current.currentTime = 0;
     }
   };
+
   const enterMouseHandler8 = () => {
     if (clip8.current) {
       clip8.current.play();
@@ -126,101 +127,199 @@ export default function SkyNight() {
                 muted
               />
               <div className="card-data">
-                  <span className="card-description"></span>
-                  <h2 className="card-title"></h2>
-                  <a href="#" className="card-button"></a>
+                <span className="card-description">Html / Css / js</span>
+                <h2 className="card-title text-2xl">Game Project</h2>
+                <div className="container-btn flex gap-2 items-center">
+                  <a href="#" className="card-button">
+                    Read more
+                  </a>
+                  <Image className="icon-btn" src={rightIcon} width={20} />
+                </div>
               </div>
             </article>
           </div>
 
-          <video
-            onMouseEnter={enterMouseHandler2}
-            onMouseLeave={leaveMouseHandler2}
-            ref={clip2}
-            src="/videos/apiList.mp4"
-            className="clip rounded-2xl object-cover"
-            width={700}
-            height={350}
-            muted
-          />
-        </div>
-
-        <div className="flex justify-between px-8 mb-64">
-          <video
-            onMouseEnter={enterMouseHandler3}
-            onMouseLeave={leaveMouseHandler3}
-            ref={clip3}
-            src="/videos/game2.mp4"
-            className="clip rounded-2xl object-cover"
-            width={700}
-            height={350}
-            poster="/img/game2.png"
-            muted
-          />
-          <video
-            onMouseEnter={enterMouseHandler4}
-            onMouseLeave={leaveMouseHandler4}
-            ref={clip4}
-            src="/videos/keyhan.mp4"
-            className="clip rounded-2xl object-cover"
-            width={700}
-            height={350}
-            muted
-          />
-        </div>
-
-        <div className="flex justify-between px-8 mb-64">
-          <video
-            onMouseEnter={enterMouseHandler5}
-            onMouseLeave={leaveMouseHandler5}
-            ref={clip5}
-            src="/videos/portfolioSH.mp4"
-            className="clip rounded-2xl object-cover "
-            width={700}
-            height={350}
-            muted
-          />
-          <video
-            onMouseEnter={enterMouseHandler6}
-            onMouseLeave={leaveMouseHandler6}
-            ref={clip6}
-            src="/videos/fastFood.mp4"
-            className="clip rounded-2xl object-cover"
-            width={700}
-            height={350}
-            poster="/img/fastfood.png"
-            muted
-          />
-        </div>
-
-        <div className="flex justify-between px-8 mb-64">
-          <div
-            onMouseEnter={enterMouseHandler7}
-            onMouseLeave={leaveMouseHandler7}
-            className="relative container-clip"
-          >
-            <video
-              ref={clip7}
-              src="/videos/travel.mp4"
-              className=" clip rounded-2xl object-cover"
-              width={700}
-              height={350}
-              muted
-            />
-
-            <div className="bg-clip clip rounded-2xl"></div>
+          <div className="card-container">
+            <article className="card-article">
+              <video
+                onMouseEnter={enterMouseHandler2}
+                onMouseLeave={leaveMouseHandler2}
+                ref={clip2}
+                src="/videos/apiList.mp4"
+                className="clip rounded-2xl object-cover"
+                width={700}
+                height={350}
+                muted
+              />
+              <div className="card-data card-data-two">
+                <span className="card-description">Css / React</span>
+                <h2 className="card-title text-2xl">Api List</h2>
+                <div className="container-btn flex gap-2 items-center">
+                  <a href="#" className="card-button">
+                    Read more
+                  </a>
+                  <Image className="icon-btn" src={rightIcon} width={20} />
+                </div>
+              </div>
+            </article>
           </div>
+        </div>
 
-          <video
-            onMouseEnter={enterMouseHandler8}
-            onMouseLeave={leaveMouseHandler8}
-            ref={clip8}
-            src="/videos/basketshop.mp4"
-            className=" clip rounded-2xl object-cover"
-            width={700}
-            height={350}
-            muted
-          />
+        <div className="flex justify-between px-8 mb-64">
+          <div className="card-container">
+            <article className="card-article">
+              <video
+                onMouseEnter={enterMouseHandler3}
+                onMouseLeave={leaveMouseHandler3}
+                ref={clip3}
+                src="/videos/game2.mp4"
+                className="clip rounded-2xl object-cover"
+                width={700}
+                height={350}
+                poster="/img/game2.png"
+                muted
+              />
+              <div className="card-data card-data-two">
+                <span className="card-description">Taillwind Css / Alpine / js</span>
+                <h2 className="card-title text-2xl">Game project two</h2>
+                <div className="container-btn flex gap-2 items-center">
+                  <a href="#" className="card-button">
+                    Read more
+                  </a>
+                  <Image className="icon-btn" src={rightIcon} width={20} />
+                </div>
+              </div>
+            </article>
+          </div>
+          <div className="card-container">
+            <article className="card-article">
+              <video
+                onMouseEnter={enterMouseHandler4}
+                onMouseLeave={leaveMouseHandler4}
+                ref={clip4}
+                src="/videos/keyhan.mp4"
+                className="clip rounded-2xl object-cover"
+                width={700}
+                height={350}
+                muted
+              />
+              <div className="card-data">
+                <span className="card-description">Html / Css / Flex / js</span>
+                <h2 className="card-title text-2xl">Galaxy Project</h2>
+                <div className="container-btn flex gap-2 items-center">
+                  <a href="#" className="card-button">
+                    Read more
+                  </a>
+                  <Image className="icon-btn" src={rightIcon} width={20} />
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="flex justify-between px-8 mb-64">
+          <div className="card-container">
+            <article className="card-article">
+              <video
+                onMouseEnter={enterMouseHandler5}
+                onMouseLeave={leaveMouseHandler5}
+                ref={clip5}
+                src="/videos/portfolioSH.mp4"
+                className="clip rounded-2xl object-cover "
+                width={700}
+                height={350}
+                muted
+              />
+              <div className="card-data">
+                <span className="card-description">React / Next.js / Taillwind / AOS / Jango</span>
+                <h2 className="card-title text-2xl">Portfolio and Shoppoing project</h2>
+                <div className="container-btn flex gap-2 items-center">
+                  <a href="#" className="card-button">
+                    Read more
+                  </a>
+                  <Image className="icon-btn" src={rightIcon} width={20} />
+                </div>
+              </div>
+            </article>
+          </div>
+          <div className="card-container">
+            <article className="card-article">
+              <video
+                onMouseEnter={enterMouseHandler6}
+                onMouseLeave={leaveMouseHandler6}
+                ref={clip6}
+                src="/videos/fastFood.mp4"
+                className="clip rounded-2xl object-cover"
+                width={700}
+                height={350}
+                poster="/img/fastfood.png"
+                muted
+              />
+              <div className="card-data card-data-two">
+                <span className="card-description">React / Next.js / AOS</span>
+                <h2 className="card-title text-2xl">Fast Food Project</h2>
+                <div className="container-btn flex gap-2 items-center">
+                  <a href="#" className="card-button">
+                    Read more
+                  </a>
+                  <Image className="icon-btn" src={rightIcon} width={20} />
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="flex justify-between px-8 mb-64">
+          
+        <div className="card-container">
+              <article className="card-article">
+                <video
+                  onMouseEnter={enterMouseHandler8}
+                  onMouseLeave={leaveMouseHandler8}
+                  ref={clip8}
+                  src="/videos/basketshop.mp4"
+                  className=" clip rounded-2xl object-cover"
+                  width={700}
+                  height={350}
+                  muted
+                />
+                <div className="card-data card-data-two">
+                  <span className="card-description">React / Redux</span>
+                  <h2 className="card-title text-2xl">Shopping cart Project</h2>
+                  <div className="container-btn flex gap-2 items-center">
+                    <a href="#" className="card-button">
+                      Read more
+                    </a>
+                    <Image className="icon-btn" src={rightIcon} width={20} />
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div className="card-container">
+              <article className="card-article">
+                <video
+                onMouseEnter={enterMouseHandler7}
+                onMouseLeave={leaveMouseHandler7}
+                  ref={clip7}
+                  src="/videos/travel.mp4"
+                  className=" clip rounded-2xl object-cover"
+                  width={700}
+                  height={350}
+                  muted
+                />
+                <div className="card-data">
+                  <span className="card-description">Html / Css / Grid</span>
+                  <h2 className="card-title text-2xl">Travel Project</h2>
+                  <div className="container-btn flex gap-2 items-center">
+                    <a href="#" className="card-button">
+                      Read more
+                    </a>
+                    <Image className="icon-btn" src={rightIcon} width={20} />
+                  </div>
+                </div>
+              </article>
+            </div>
         </div>
       </div>
     </>
